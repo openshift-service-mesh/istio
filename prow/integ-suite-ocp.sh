db100e5aaeea4ee6df92552e3f5ac196b423bf3f
Francisco Herrera <fjglira@gmail.com>
2024-04-26 13:40:16 +0200

    OSSM-4505 Add script to run integration test againt OCP cluster (#4)
    * Initial changes to run istio integration test on OCP
    
    Update integ suite ocp script
    
    Working script to run the integration test over OCP clusters
    
    Adding an improvement in skip test
    
    Modify script based on the use of a common script to setup registry-s
    
    Delete unused rolebinding file
    
    Signed-off-by: frherrer <frherrer@redhat.com>
    
    * Adding improvements in the run script over OCP
    
    ---------
    
    Signed-off-by: frherrer <frherrer@redhat.com>

---
+++ prow/integ-suite-ocp.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# Copyright 2019 Istio Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script is used to run the integration tests on OpenShift.
+# Usage: ./integ-suite-ocp.sh TEST_SUITE SKIP_TESTS, example: /prow/integ-suite-ocp.sh telemetry "TestClientTracing|TestServerTracing"
+# TEST_SUITE: The test suite to run. Default is "pilot". Available options are "pilot", "security", "telemetry", "helm".
+# TODO: Use the same arguments as integ-suite.kind.sh uses
+
+WD=$(dirname "$0")
+ROOT=$(dirname "$WD")
+WD=$(cd "$WD"; pwd)
+TIMEOUT=300
+export NAMESPACE="${NAMESPACE:-"istio-system"}"
+export TAG="${TAG:-"istio-testing"}"
+SKIP_TESTS="${2:-""}"
+TEST_SUITE="${1:-"pilot"}"
+
+# Exit immediately for non zero status
+set -e
+# Check unset variables
+set -u
+# Print commands
+set -x
+
+# shellcheck source=common/scripts/kind_provisioner.sh
+source "${ROOT}/prow/setup/ocp_setup.sh"
+
+build_images() {
+    # Build just the images needed for tests
+    targets="docker.pilot docker.proxyv2 docker.install-cni "
+
+    # Integration tests are always running on local architecture (no cross compiling), so find out what that is.
+    arch="linux/amd64"
+    if [[ "$(uname -m)" == "aarch64" ]]; then
+        arch="linux/arm64"
+    fi
+
+    # use ubuntu:jammy to test vms by default
+    nonDistrolessTargets="docker.app docker.app_sidecar_ubuntu_jammy docker.ext-authz "
+
+    DOCKER_ARCHITECTURES="${arch}"  DOCKER_BUILD_VARIANTS="${VARIANT:-default}" DOCKER_TARGETS="${targets} ${nonDistrolessTargets}" make dockerx.pushx
+}
+
+# Setup the internal registry for ocp cluster
+setup_internal_registry
+
+# Build and push the images to the internal registry
+build_images
+
+# Run the integration tests
+echo "Running integration tests"
+
+# Set the HUB to the internal registry svc URL to avoid the need to authenticate to pull images
+HUB="image-registry.openshift-image-registry.svc:5000/${NAMESPACE}"
+
+# Build the base command and store it in a variable.
+# TODO: execute the test by running make target. Do we need first to add a skip flag to the make target to be able to skip failing test on OCP
+# All the flags are needed to run the integration tests on OCP
+base_cmd="go test -p 1 -v -count=1 -tags=integ -vet=off -timeout 60m ./tests/integration/${TEST_SUITE}/... \
+--istio.test.ci \
+--istio.test.pullpolicy=IfNotPresent \
+--istio.test.work_dir=result \
+--istio.test.skipTProxy=true \
+--istio.test.skipVM=true \
+--istio.test.kube.helm.values=profile=openshift,global.platform=openshift \
+--istio.test.istio.enableCNI=true \
+--istio.test.hub=\"${HUB}\" \
+--istio.test.tag=\"${TAG}\""
+
+# Check if SKIP_TESTS is non-empty and append the -skip flag if it is.
+if [ -n "${SKIP_TESTS}" ]; then
+  base_cmd+=" -skip '${SKIP_TESTS}'"
+fi
+
+# Execute the command.
+eval "$base_cmd"
