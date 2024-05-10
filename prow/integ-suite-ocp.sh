35f8c51add241861d11520d96d65e0b4626184de
frherrer <frherrer@redhat.com>
2024-05-10 12:09:46 +0000

    update comment

--- prow/integ-suite-ocp.sh
+++ prow/integ-suite-ocp.sh
@@ -48,7 +48,7 @@ build_images() {
         arch="linux/arm64"
     fi
 
-    # use ubuntu:jammy to test vms by default
+    # use ubuntu:noble to test vms by default
     nonDistrolessTargets="docker.app docker.app_sidecar_ubuntu_noble docker.ext-authz "
 
     DOCKER_ARCHITECTURES="${arch}"  DOCKER_BUILD_VARIANTS="${VARIANT:-default}" DOCKER_TARGETS="${targets} ${nonDistrolessTargets}" make dockerx.pushx
