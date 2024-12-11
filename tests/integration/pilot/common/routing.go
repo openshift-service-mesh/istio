3169415d93cf6c601030b3584df058866789c1f0
Brian Mangoenpawiro <bmangoen@gmail.com>
2024-12-11 15:01:25 +0100

    Fix go linter errors (#230)
    Signed-off-by: bmangoen <9867932+bmangoen@users.noreply.github.com>
    Co-authored-by: bmangoen <9867932+bmangoen@users.noreply.github.com>

--- tests/integration/pilot/common/routing.go
+++ tests/integration/pilot/common/routing.go
@@ -4205,7 +4205,7 @@ spec:
 			},
 			Check: check.Status(http.StatusOK),
 			// Retry with a longer duration until the AWS LB is available
-			//TODO: Fix it properly as recommended https://github.com/istio/istio/pull/54244#pullrequestreview-2489497585
+			// TODO: Fix it properly as recommended https://github.com/istio/istio/pull/54244#pullrequestreview-2489497585
 			Retry: echo.Retry{
 				Options: []retry.Option{retry.Timeout(2 * time.Minute)},
 			},
@@ -4239,7 +4239,7 @@ spec:
 			},
 			Check: check.Status(http.StatusOK),
 			// Retry with a longer duration until the AWS LB is available
-			//TODO: Fix it properly as recommended https://github.com/istio/istio/pull/54244#pullrequestreview-2489497585
+			// TODO: Fix it properly as recommended https://github.com/istio/istio/pull/54244#pullrequestreview-2489497585
 			Retry: echo.Retry{
 				Options: []retry.Option{retry.Timeout(2 * time.Minute)},
 			},
