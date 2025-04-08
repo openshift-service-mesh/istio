e479c4f186327d2f06145803885d7c85d43fd7e9
Yann Liu <yannliu@redhat.com>
2025-04-08 12:11:18 +0800

    [release-1.26] Add flag to be compliant with attribute handling behavior in the specification (#341)
    Signed-off-by: Yann Liu <yannliu@redhat.com>

--- pilot/pkg/features/experimental.go
+++ pilot/pkg/features/experimental.go
@@ -138,6 +138,8 @@ var (
 	EnableAlphaGatewayAPI     = env.Register(EnableAlphaGatewayAPIName, false,
 		"If this is set to true, support for alpha APIs in the Kubernetes gateway-api (github.com/kubernetes-sigs/gateway-api) will "+
 			" be enabled. In addition to this being enabled, the gateway-api CRDs need to be installed.").Get()
+	EnableGatewayAPICopyLabelsAnnotations = env.Register("PILOT_ENABLE_GATEWAY_API_COPY_LABELS_ANNOTATIONS", true,
+		"If this is set to false, istiod will not copy any attributes from the Gateway resource onto its related Deployment resources.").Get()
 
 	EnableGatewayAPIStatus = env.Register("PILOT_ENABLE_GATEWAY_API_STATUS", true,
 		"If this is set to true, gateway-api resources will have status written to them").Get()
