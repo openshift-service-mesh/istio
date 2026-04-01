bcaf99b9ded6735e558b820ae40cdbbda0f6fe31
Matej Kralik <mkralik@redhat.com>
2026-04-01 09:51:26 +0200

    [release-1.26] [OSSM-12848] Fix pilot/gatewayinstance subsuite when sail operator is used#706 (#707)
    * Do not use a default revision in gatewayinstance tests (#501)
    
    The test setup was creating two revisions where one was called default.
    In Istio 1.27 it resulted in two conflicting default tags which caused
    the echo workloads to be injected by incorrect control plane (gateway)
    which fails as it's not using CNI.
    
    * [OSSM-12848] Fix pilot/gatewayinstance subsuite when sail operator is used (#648)
    
    Signed-off-by: mkralik3 <mkralik@redhat.com>
    
    ---------
    
    Signed-off-by: mkralik3 <mkralik@redhat.com>
    Co-authored-by: Filip Brychta <fbrychta@redhat.com>

--- tests/integration/pilot/gatewayinstance/main_test.go
+++ tests/integration/pilot/gatewayinstance/main_test.go
@@ -99,12 +99,13 @@ values:
 			cfg.Values["global.istioNamespace"] = meshInstanceNS.Name()
 			cfg.SystemNamespace = meshInstanceNS.Name()
 			cfg.ControlPlaneValues = fmt.Sprintf(`
-namespace: %s`, meshInstanceNS.Name())
+namespace: %s
+revision: mesh`, meshInstanceNS.Name())
 		})).
 		SetupParallel(
 			// application namespaces are labeled according to the required control plane ownership.
-			namespace.Setup(&echoNS, namespace.Config{Prefix: "echo1", Inject: true, Revision: "", Labels: nil}),
-			namespace.Setup(&externalNS, namespace.Config{Prefix: "external", Inject: false})).
+			namespace.Setup(&echoNS, namespace.Config{Prefix: "echo1", Inject: true, Revision: "mesh", Labels: map[string]string{"istio-instance": "mesh"}}),
+			namespace.Setup(&externalNS, namespace.Config{Prefix: "external", Inject: false, Labels: map[string]string{"istio-instance": "gateway"}})).
 		SetupParallel(
 			deployment.SetupSingleNamespace(&apps, deployment.Config{
 				Namespaces: []namespace.Getter{
